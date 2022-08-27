65c6ee1d8cb2d733df8c8643f2bd09cc643043e9
Noman Waseem <nomanwaseem111@gmail.com>
2022-08-27 20:36:04 +0500

    changes

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 const app = express()
 app.use(express.json())
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 3000
 
 const userSchema = new mongoose.Schema({
     name: {type : String, required : true},
